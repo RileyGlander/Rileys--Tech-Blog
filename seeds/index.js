595924e1cfe7108e5f58363e3761bed81ef88167
RileyGlander <142702948+RileyGlander@users.noreply.github.com>
2023-12-09 16:30:23 -0600

    updated index.js, User.js and server.js

---
+++ seeds/index.js
@@ -0,0 +1,37 @@
+const sequelize = require('../config/connection');
+const { User } = require('../models');
+
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  process.exit(0);
+};
+
+seedDatabase();
+
+
+
+// const seedDatabase = async () => {
+//     await sequelize.sync({ force: true });
+  
+//     const drivers = await Driver.bulkCreate(driverSeedData);
+  
+//     for (const { id } of drivers) {
+//       const newLicense = await License.create({
+//         driver_id: id,
+//       });
+//     }
+  
+//     for (const car of carSeedData) {
+//       const newCar = await Car.create({
+//         ...car,
+//         // Attach a random driver ID to each car
+//         driver_id: drivers[Math.floor(Math.random() * drivers.length)].id,
+//       });
+//     }
--- server.js
+++ server.js
@@ -1,12 +1,49 @@
+const path = require('path')
 const express = require('express')
 const exphbs = require('express-handlebars');
 const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const mysql = require ('mysql2');
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize,
+    }),
+};
+
+// const db = mysql.createConnection(
+//     {
+//         host: 'localhost',
+//         user: 'root',
+//         password: '',
+//         database: 'user_db'
+//     }
+// )
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app,set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes)
+
+sequelize.sync({force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
