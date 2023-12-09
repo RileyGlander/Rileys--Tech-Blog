e560edd20f1e6443def456ac943cde1d7ab45ab2
RileyGlander <142702948+RileyGlander@users.noreply.github.com>
2023-12-09 14:22:03 -0600

    added necessary files and paackages and started working on connection.js, schema.sql, .env and server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
