4d15d3752384893e1d585ce3d205c156dead18f3
RileyGlander <142702948+RileyGlander@users.noreply.github.com>
2023-12-10 14:59:05 -0600

    updated auth.js and helpers.js with authorization code and functions for formatting time and date

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,3 +1,10 @@
-module.exports = {
 
-}
+    function format_time (date) {
+      return date.toLocaleTimeString();
+    }
+    function format_date (date) {
+      return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${
+        new Date(date).getFullYear() + 5
+      }`;
+    }
+ module.exports = { format_time, format_date };
